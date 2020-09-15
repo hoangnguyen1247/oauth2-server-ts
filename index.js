ef5aae2f6309f5f997d669ae5e37d24cbb001697
Hoang <hoangnguyen1247@gmail.com>
2020-09-15 20:55:08 +0700

    correct index file and bump version

---
+++ index.js
@@ -0,0 +1,10 @@
+export * from './lib/errors';
+export * from './lib/grant-types';
+export * from './lib/handlers';
+export * from './lib/interfaces';
+export * from './lib/response-types';
+export * from './lib/token-types';
+export { Request } from './lib/request';
+export { Response } from './lib/response';
+export { OAuth2Server } from './lib/server';
+export * from './lib/validator/is';
